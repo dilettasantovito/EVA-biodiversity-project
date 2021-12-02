baee78cee336dfa2ff879572e2fec860d1c90a31
dilettasantovito <80915476+dilettasantovito@users.noreply.github.com>
2021-12-02 12:39:26 +0100

    Add files via upload

---
+++ match_euromed.R
@@ -0,0 +1,193 @@
+#### Loading data ####
+
+# Euro+Med data
+
+euromed_table <- read.csv("EuroMed.csv",
+  header = TRUE,
+  sep = ";"
+)
+
+euromed <- euromed_table[-grep("?", euromed_table$TaxonConcept, fixed = T), ]
+
+# EVA data
+
+species <- read_rds("species.RDS")
+
+vascular_species <- species %>%
+  filter(Taxon.group == "Vascular plant")
+
+# Total unique species
+
+vascular_species_distinct <- vascular_species %>%
+  filter(Taxon.group == "Vascular plant") %>%
+  distinct(Matched.concept)
+
+# Species and occurrences matching Euro+Med
+
+vascular_in_euromed <- vascular_species_distinct$Matched.concept %in%
+  euromed$TaxonConcept
+
+vascular_match <- vascular_species_distinct[vascular_in_euromed, ] %>%
+  as_tibble() %>%
+  rename(ScientificName = value) %>%
+  mutate(Matched.concept = ScientificName)
+
+match_in_occurrences <- vascular_species %>%
+  filter(Matched.concept %in% vascular_match$Matched.concept)
+
+# Non-matching species and occurrences
+
+vascular_nonmatch <- vascular_species_distinct[!vascular_in_euromed, ] %>%
+  as_tibble() %>%
+  rename(ScientificName = value) %>%
+  mutate(Matched.concept = ScientificName)
+
+vascular_nonmatch_occurrences <- vascular_species %>%
+  filter(Matched.concept %in% vascular_nonmatch$Matched.concept)
+
+#### Filter ####
+
+# I filter out non-matching occurrences containing mistakes or missing information
+# and correct the remaining ones.
+# I add a column named ScientificName2 containing the regular expressions that
+# I'll need in order to match the remaining species and occurrences with Euro+Med.
+
+vascular_filter <- vascular_nonmatch %>%
+  filter(
+    !grepl("species", fixed = TRUE, ScientificName),
+    !grepl("/", fixed = TRUE, ScientificName),
+    !grepl("sect.", fixed = TRUE, ScientificName),
+    !grepl("ser.", fixed = TRUE, ScientificName),
+    !grepl("Ser.", fixed = TRUE, ScientificName),
+    !grepl(" et ", fixed = TRUE, ScientificName),
+    !grepl("(group)", fixed = TRUE, ScientificName),
+    !grepl("(Group)", fixed = TRUE, ScientificName),
+    !grepl("+", fixed = TRUE, ScientificName),
+    !grepl("subg.", fixed = TRUE, ScientificName),
+    !grepl("(excl", fixed = TRUE, ScientificName),
+    !grepl("Subs.", fixed = TRUE, ScientificName),
+    !grepl("aggr.", fixed = TRUE, ScientificName),
+    !grepl("gr.", fixed = TRUE, ScientificName),
+    !grepl("subgen.", fixed = TRUE, ScientificName)
+  ) %>%
+  mutate(
+    ScientificName = gsub(" x ", " ", fixed = TRUE, ScientificName),
+    ScientificName = gsub(" s.l.", "", fixed = TRUE, ScientificName),
+    ScientificName = gsub(" sl.", "", fixed = TRUE, ScientificName),
+    ScientificName = gsub(" s.s.", "", fixed = TRUE, ScientificName),
+    ScientificName = gsub(" f. ", " ", fixed = TRUE, ScientificName),
+    ScientificName = gsub(" cf.", " ", fixed = TRUE, ScientificName),
+    ScientificName = gsub(" a. ", " ", fixed = TRUE, ScientificName),
+    ScientificName = gsub(" A. ", " ", fixed = TRUE, ScientificName),
+    ScientificName = gsub(" (hybr.)", "", fixed = TRUE, ScientificName),
+    ScientificName = gsub("x-", "", fixed = TRUE, ScientificName),
+    ScientificName = gsub(" (dood)", "", fixed = TRUE, ScientificName)
+  ) %>%
+  mutate(ScientificName = word(ScientificName, 1, 2)) %>%
+  mutate(ScientificName2 = paste("^",
+    word(ScientificName, 1),
+    ".*",
+    word(ScientificName, 2),
+    "$",
+    sep = ""
+  ))
+
+# Filtered out species and occurrences
+
+filteredout_species <- vascular_nonmatch %>%
+  filter(grepl("species", fixed = TRUE, ScientificName) |
+    grepl("/", fixed = TRUE, ScientificName) |
+    grepl("sect.", fixed = TRUE, ScientificName) |
+    grepl("ser.", fixed = TRUE, ScientificName) |
+    grepl("Ser.", fixed = TRUE, ScientificName) |
+    grepl(" et ", fixed = TRUE, ScientificName) |
+    grepl("(group)", fixed = TRUE, ScientificName) |
+    grepl("(Group)", fixed = TRUE, ScientificName) |
+    grepl("+", fixed = TRUE, ScientificName) |
+    grepl("subg.", fixed = TRUE, ScientificName) |
+    grepl("(excl", fixed = TRUE, ScientificName) |
+    grepl("Subs.", fixed = TRUE, ScientificName) |
+    grepl("aggr.", fixed = TRUE, ScientificName) |
+    grepl("gr.", fixed = TRUE, ScientificName) |
+    grepl("subgen.", fixed = TRUE, ScientificName))
+
+filteredout_occurrences <- vascular_species %>%
+  filter(Matched.concept %in% filteredout_species$Matched.concept)
+
+regex_nonmatch <- vascular_nonmatch_occurrences %>%
+  filter(!Matched.concept %in% filteredout_species$Matched.concept)
+
+#### Matching the fixed occurrences with Euro+Med ####
+
+# I retrieve euromed$TaxonConcept codes using ScientificName2 and use them to
+# match the fixed occurrences and species with Euro+Med
+
+euromed_codes <- pbmclapply(vascular_filter$ScientificName2,
+  function(x) grep(x, euromed$TaxonConcept),
+  mc.cores = 2
+)
+
+euromed_codes2 <- pbmclapply(vascular_match$ScientificName,
+  function(x) grep(x, euromed$TaxonConcept),
+  mc.cores = 2
+)
+
+euromed_code <- euromed_codes %>%
+  lapply(., function(x) x[1]) %>%
+  do.call(c, .)
+
+euromed_code2 <- euromed_codes2 %>%
+  lapply(., function(x) x[1]) %>%
+  do.call(c, .)
+
+vascular_filter2 <- vascular_filter %>%
+  mutate(
+    Code = euromed_code,
+    TaxonConcept = euromed$TaxonConcept[euromed_code]
+  )
+
+vascular_match2 <- vascular_match %>%
+  mutate(
+    Code = euromed_code2,
+    TaxonConcept = euromed$TaxonConcept[euromed_code2]
+  )
+
+regex_species <- vascular_filter2 %>%
+  drop_na()
+
+regex_occurrences <- vascular_species %>%
+  filter(Matched.concept %in% regex_species$Matched.concept)
+
+# Total species and occurrences matching Euro+Med
+
+match_species <- vascular_filter2 %>%
+  select(Matched.concept, ScientificName, TaxonConcept) %>%
+  rbind(select(vascular_match2, Matched.concept, ScientificName, TaxonConcept)) %>%
+  drop_na()
+
+match_species2 <- match_species %>%
+  inner_join(euromed) %>%
+  distinct(Matched.concept, .keep_all = TRUE) %>%
+  select(Matched.concept, ScientificName, TaxonConcept, TaxonConceptID)
+
+match_occurrences <- vascular_species %>%
+  filter(Matched.concept %in% match_species2$Matched.concept)
+
+match_occurrences2 <- vascular_species %>%
+  inner_join(match_species2)
+
+#### Non-matching species and occurrences ####
+
+nonmatch_species <- vascular_filter2 %>%
+  filter(is.na(TaxonConcept)) %>%
+  select(Matched.concept, ScientificName)
+
+nonmatch_occurrences <- vascular_species %>%
+  filter(Matched.concept %in% nonmatch_species$Matched.concept)
+
+write.table(match_occurrences2,
+  "match_occurrences.csv",
+  sep = ";",
+  dec = ",",
+  col.names = TRUE
+)
---
+++ plot_inout_wip.R
@@ -0,0 +1,200 @@
+#### Packages ####
+
+library(tidyverse)
+library(ggpubr)
+
+#### Loading data ####
+
+plot_inout <- read.csv("plot_inout_n2k.csv",
+  header = TRUE,
+  sep = ","
+) %>%
+  rename(PlotObservationID = id) %>%
+  arrange(PlotObservationID)
+
+match_species <- read.csv("match_species.csv",
+  header = TRUE,
+  sep = ";",
+  dec = ","
+)
+
+match_occurrences <- read.csv("match_occurrences.csv",
+  header = TRUE,
+  sep = ";",
+  dec = ","
+)
+
+header2 <- data.table::fread("header2.csv", quote = "")
+header <- header2 %>%
+  select(PlotObservationID, Country, `Relevé area (m²)`, `Expert System`, Bioreg_EEA)
+
+#### Plot in/out ####
+
+plot_inout_id <- plot_inout %>%
+  left_join(header, by = "PlotObservationID")
+
+plot_inout_occurrences <- match_occurrences$PlotObservationID %in%
+  plot_inout_id$PlotObservationID
+
+plot_inout_occurrences2 <- match_occurrences[plot_inout_occurrences, ] %>%
+  select(PlotObservationID, Matched.concept, ScientificName, Cover..) %>%
+  left_join(plot_inout_id, by = "PlotObservationID") %>%
+  mutate(HabitatType = substr(`Expert System`, 0, 1)) %>%
+  filter(!grepl("uncertain", n2k_inout)) %>%
+  as_tibble()
+
+cols <- c("Country", "Bioreg_EEA", "n2k_inout", "HabitatType")
+plot_inout_occurrences2[cols] <- map_dfr(plot_inout_occurrences2[cols], as.factor)
+
+#### Divide the dataset in countries, biogeographic regions and habitat types ####
+
+plot_inout_countries <- split(plot_inout_occurrences2, plot_inout_occurrences2$Country)
+plot_inout_bioreg <- split(plot_inout_occurrences2, plot_inout_occurrences2$Bioreg_EEA)
+plot_inout_habitat <- split(plot_inout_occurrences2, plot_inout_occurrences2$HabitatType)
+
+#### Number of species ####
+
+species_countries <- plot_inout_occurrences2 %>%
+  group_by(Country, n2k_inout) %>%
+  summarize(Species_richness = n_distinct(ScientificName))
+
+species_bioreg <- plot_inout_occurrences2 %>%
+  filter(!(Bioreg_EEA %in% "")) %>%
+  group_by(Bioreg_EEA, n2k_inout) %>%
+  summarize(Species_richness = n_distinct(ScientificName))
+
+species_habitat <- plot_inout_occurrences2 %>%
+  filter(!(HabitatType %in% c("", "?"))) %>%
+  group_by(HabitatType, n2k_inout) %>%
+  summarize(Species_richness = n_distinct(ScientificName))
+
+#### Area ####
+
+area_countries <- plot_inout_occurrences2 %>%
+  group_by(n2k_inout) %>%
+  summarize("Area" = sum(`Relevé area (m²)`, na.rm = TRUE))
+
+area_bioreg <- plot_inout_occurrences2 %>%
+  filter(!(Bioreg_EEA %in% "")) %>%
+  group_by(n2k_inout) %>%
+  summarize("Area" = sum(`Relevé area (m²)`, na.rm = TRUE))
+
+area_habitat <- plot_inout_occurrences2 %>%
+  filter(!(HabitatType %in% c("", "?"))) %>%
+  group_by(n2k_inout) %>%
+  summarize("Area" = sum(`Relevé area (m²)`, na.rm = TRUE))
+
+#### Number of plots ####
+
+nplot_countries <- plot_inout_occurrences2 %>%
+  group_by(n2k_inout) %>%
+  summarize("N° plot" = length(PlotObservationID))
+
+nplot_bioreg <- plot_inout_occurrences2 %>%
+  filter(!(Bioreg_EEA %in% "")) %>%
+  group_by(n2k_inout) %>%
+  summarize("N° plot" = length(PlotObservationID))
+
+nplot_habitat <- plot_inout_occurrences2 %>%
+  filter(!(HabitatType %in% c("", "?"))) %>%
+  group_by(n2k_inout) %>%
+  summarize("N° plot" = length(PlotObservationID))
+
+#### Summaries ####
+
+summary_countries <- plot_inout_occurrences2 %>%
+  group_by(Country, n2k_inout) %>%
+  summarise(
+    sr = n_distinct(ScientificName),
+    area = sum(`Relevé area (m²)`, na.rm = TRUE) / 1e6,
+    n_plot = length(PlotObservationID)
+  )
+
+summary_bioreg <- plot_inout_occurrences2 %>%
+  filter(!(Bioreg_EEA %in% "")) %>%
+  group_by(Bioreg_EEA, n2k_inout) %>%
+  summarise(
+    sr = n_distinct(ScientificName),
+    area = sum(`Relevé area (m²)`, na.rm = TRUE) / 1e6,
+    n_plot = length(PlotObservationID)
+  )
+
+summary_habitat <- plot_inout_occurrences2 %>%
+  filter(!(HabitatType %in% c("", "?"))) %>%
+  group_by(HabitatType, n2k_inout) %>%
+  summarise(
+    sr = n_distinct(ScientificName),
+    area = sum(`Relevé area (m²)`, na.rm = TRUE) / 1e6,
+    n_plot = length(PlotObservationID)
+  )
+
+#### Plot theme ####
+
+theme_set(theme_void(base_family = "Roboto"))
+theme_update(
+  axis.text.x = element_text(
+    color = "black", face = "plain", size = 15,
+    margin = margin(t = 6)
+  ),
+  axis.text.y = element_text(
+    color = "black", size = 15, hjust = 1,
+    margin = margin(r = 6), family = "Roboto Mono"
+  ),
+  axis.line.x = element_line(color = "black", size = 1),
+  panel.grid.major.y = element_line(color = "grey90", size = .6),
+  plot.background = element_rect(fill = "white", color = "white"),
+  plot.margin = margin(rep(20, 4))
+)
+
+mypal <- c("deepskyblue2", "dodgerblue4")
+options(ggrepel.max.overlaps = Inf)
+
+#### Plots ####
+
+# Countries #
+
+countries_p1 <- ggplot(species_countries, aes(x = n2k_inout, y = Species_richness, color = n2k_inout, fill = n2k_inout)) +
+  scale_color_manual(values = mypal, guide = "none") +
+  scale_fill_manual(values = mypal, guide = "none") +
+  geom_boxplot(aes(fill = n2k_inout, fill = after_scale(colorspace::lighten(fill, .7))),
+    size = 1.5, outlier.shape = NA
+  ) +
+  geom_line(aes(group = Country), alpha = 0.5, colour = "darkgrey") +
+  stat_compare_means(paired = TRUE, method = "wilcox.test", label.x.npc = 0.3, label.y.npc = 1) +
+  geom_label(aes(label = Country), position = "identity", size = 3, color = "white")
+
+countries_p2 <- ggplot(summary_countries) +
+  geom_point(mapping = aes(x = n2k_inout, y = sr, colour = area, size = n_plot))
+
+countries_table <- area_countries %>%
+  ggtexttable(rows = NULL)
+
+ggarrange(countries_p1, countries_p2, countries_table,
+  labels = c("A", "B", "C"),
+  ncol = 3, nrow = 1
+)
+
+# Biogeographic regions #
+
+bioreg_p1 <- ggplot(species_bioreg, aes(x = n2k_inout, y = Species_richness, color = n2k_inout, fill = n2k_inout)) +
+  scale_color_manual(values = mypal, guide = "none") +
+  scale_fill_manual(values = mypal, guide = "none") +
+  geom_boxplot(aes(fill = n2k_inout, fill = after_scale(colorspace::lighten(fill, .7))),
+    size = 1.5, outlier.shape = NA
+  ) +
+  geom_line(aes(group = Bioreg_EEA), alpha = 0.5, colour = "darkgrey") +
+  # stat_compare_means(paired = TRUE, method = "wilcox.test", label.x.npc = 0.3, label.y.npc = 1) +
+  geom_label(aes(label = Bioreg_EEA), position = "identity", size = 3, color = "white")
+
+bioreg_p2 <- ggplot(summary_bioreg) +
+  geom_point(mapping = aes(x = n2k_inout, y = sr, colour = area, size = n_plot))
+
+bioreg_table <- area_bioreg %>%
+  ggtexttable(rows = NULL)
+
+ggarrange(bioreg_p1, bioreg_p2, bioreg_table,
+  labels = c("A", "B", "C"),
+  ncol = 3, nrow = 1
+)
+
+# Habitat types #
